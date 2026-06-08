6f8e4969cfc1a826d04954fbba4bf20e1ffdd30e
muhamadtegarputrapratama <muhamadtegarputrapratama@smkwikrama.sch.id>
2026-06-08 19:04:14 +0700

    second commit

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -55,7 +55,7 @@ class CategoryController extends Controller
         }
 
         $request->validate([
-            'nama' => 'required|string|unique:categories,nama,' . $id, 
+            'nama' => 'required|string|unique:categories,nama,' . $id,
         ]);
 
         $category->update(['nama' => $request->nama]);
